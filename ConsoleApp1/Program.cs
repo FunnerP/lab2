1847ddce797e6d0d9275050e08a0c3517e409088
BuildTools <unconfigured@null.spigotmc.org>
2023-12-06 23:26:43 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder result = new StringBuilder();
+
+            Task[] tasks = new Task[26];
+            for (int i = 0; i < 26; i++)
+            {
+                char letter = (char)('a' + i);
+                tasks[i] = Task.Run(() =>
+                {
+                    lock (result)
+                    {
+                        result.Append(letter);
+                    }
+                });
+            }
+
+            Task.WaitAll(tasks);
+
+            Console.WriteLine(result.ToString());
+        }
+    }
